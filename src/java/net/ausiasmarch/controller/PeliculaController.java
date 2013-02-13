0fe4c9e95a6c059a0a9bbff918d11732f0698a6e
maria <maria@maria-PC>
2013-02-13 14:25:09 +0100

    Commit 1: Spring + Hibernate funcional

---
+++ src/java/net/ausiasmarch/controller/PeliculaController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.ausiasmarch.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.ausiasmarch.pojo.HibernateUtil;
+import net.ausiasmarch.pojo.Usuario;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author María Galbis
+ */
+@Controller
+public class PeliculaController {
+    
+    @RequestMapping({"/index.html"})
+    public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+       // Session session = HibernateUtil.getSessionFactory().openSession();
+       // Usuario u = (Usuario) session.get(Usuario.class, 1);
+       // System.out.println(u.getId());
+        return new ModelAndView("index");
+    }
+    
+}
